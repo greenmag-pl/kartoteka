62303ab510bd96c0cc3c5ce4edd41c67d51780af
greenmag-pl <mariusz.fron@gmail.com>
2026-07-07 09:53:59 +0200

    Add files via upload

--- kartoteka-gm/skrypty.js
+++ kartoteka-gm/skrypty.js
@@ -3,7 +3,7 @@ $(function()
 
 	let userPass = new URLSearchParams(location.search).get("klucz");
 	if (userPass === null) userPass = "";
-	$.get("klucz.txt", function(realPass)
+	$.get("klucz", function(realPass)
 	{
 		if (userPass !== realPass) $('body').html('<img src="stop.png"><p>Brak dostępu.</p>');
 
