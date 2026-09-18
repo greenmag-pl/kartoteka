6adfbea98dd34ba71a13e3865dc5f3e1b95ceec7
greenmag-pl <mariusz.fron@gmail.com>
2026-09-18 10:15:17 +0200

    Add files via upload

--- kartoteka-gm/skrypty.js
+++ kartoteka-gm/skrypty.js
@@ -2,7 +2,16 @@ function getNrAndImg(wstecz)
 {
 	$('#opis>div').eq(1).append('<span>&nbsp;</span>');
 	if (wstecz) $('#opis span').text($('body>div[id^="karta"]').index($('#karta' + $('#opis').data('nr'))) + 1 + '/' + $('body>div[id^="karta"]').length);
-	$('#opis>img').css('background-image', $('#karta' + $('#opis').data('nr')).css('background-image'));
+	const img = new Image();
+	img.onload = function()
+	{
+		$('#opis>img').css('background-image', 'url("' + img.src + '")');
+	};
+	img.onerror = function()
+	{
+		$('#opis>img').css('background-image', 'url("brak.jpg")');
+	};
+	img.src = 'karty/karta' + $('#opis').data('nr') + '/obraz.jpg';
 }
 
 function De(s)
